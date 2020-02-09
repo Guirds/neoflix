4ad843918a0e679711c063f41183d03133d3c706
Guilherme Santos <gui07015@gmail.com>
2020-02-09 17:23:05 -0300

    feat(Main/style.js) Style for Main page

---
+++ src/pages/Main/style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  margin: 30px;
+  display: flex;
+  flex-direction: row;
+  height: 50px;
+
+  input {
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #e64021;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
