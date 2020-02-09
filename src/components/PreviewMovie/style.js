9adb7fd1a6ca7831236ce2982ceed14ed204422f
Guilherme Santos <gui07015@gmail.com>
2020-02-09 20:47:57 -0300

    feat(PreviewMovie/style.js) Finish styled for components

--- src/components/PreviewMovie/style.js
+++ src/components/PreviewMovie/style.js
@@ -1,8 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.section`
+export const Container = styled.div`
   width: 250px;
-  height: 25px;
   background: #414141;
   border-radius: 5px;
+
+  margin: 10px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h4{
+    text-align: center;
+    margin-top: 10px;
+    color: #e64021;
+  }
+
+  span{
+
+    svg{
+      margin-right: 3px;
+    }
+  }
+
+  small,span{
+    margin-bottom: 5px;
+    color: #fff;
+    font-size: 12px;
+  }
+
+  img{
+    border-radius: 5px;
+  }
+
+  img,h4{
+    width: 100%;
+  }
 `;
