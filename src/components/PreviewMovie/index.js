5048fe2c0d4e101b69dd7d93c43f620946255f65
Guilherme Santos <gui07015@gmail.com>
2020-02-09 20:56:19 -0300

    fix(PreviewMovie/index) Add props and component FaStar

--- src/components/PreviewMovie/index.js
+++ src/components/PreviewMovie/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {FaStar} from 'react-icons/fa'
 import { Container } from './style';
 
-export default function PreviewMovie(img, description, ) {
+export default function PreviewMovie({title, rating, runtime, img}) {
   return (
-    <Container />
+    <Container>
+      <h4>{title}</h4>
+      <span>Rating: {rating} <FaStar color="#e64021" /></span>
+      <small>Runtime: {runtime}m</small>
+      <img src={img} alt={title}/>
+    </Container>
   );
 }
